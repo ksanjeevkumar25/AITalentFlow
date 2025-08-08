96ca2b1df9ee49e7abe635741ce7ec9d9cf16573
ksanjeevkumar25 <38934322+ksanjeevkumar25@users.noreply.github.com>
2025-08-08 20:08:09 +0100

    Delete SkillsAPP directory
    unwanted mess .. duplicate deleting

--- SkillsAPP/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace LoginApp.Models
-{
-    public class User
-    {
-        public int UserID { get; set; }
-        public int EmployeeID { get; set; }
-        public string Password { get; set; } = string.Empty;
-    }
-}
